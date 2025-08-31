58bcb39e6095cb757f955060d3a9adbb6cce1a87
Melle <118454433+mellevanderlinde@users.noreply.github.com>
2025-08-31 12:18:16 +0200

    chore: use pollUntilComplete() for readability

--- lib/lambda/drift-detection/drift.ts
+++ lib/lambda/drift-detection/drift.ts
@@ -1,24 +1,27 @@
-import type { StackDriftDetectionStatus, StackDriftStatus } from '@aws-sdk/client-cloudformation';
+import type { StackDriftStatus } from '@aws-sdk/client-cloudformation';
 import type { Region } from '../../utils/enums';
 import { setTimeout } from 'node:timers/promises';
 import { CloudFormationClient, DescribeStackDriftDetectionStatusCommand, DetectStackDriftCommand } from '@aws-sdk/client-cloudformation';
+import { assertDefined } from '../../utils/assert-defined';
 
 export async function detectDrift(stackName: string, region: Region): Promise<StackDriftStatus> {
   const client = new CloudFormationClient({ region });
-
   const detectCommand = new DetectStackDriftCommand({ StackName: stackName });
-  const { StackDriftDetectionId } = await client.send(detectCommand); // Start drift detection
+  const { StackDriftDetectionId } = await client.send(detectCommand);
+  return pollUntilComplete(client, assertDefined(StackDriftDetectionId, 'StackDriftDetectionId'));
+}
 
-  let status: StackDriftDetectionStatus | undefined = 'DETECTION_IN_PROGRESS';
-  const describeCommand = new DescribeStackDriftDetectionStatusCommand({ StackDriftDetectionId });
-  while (status === 'DETECTION_IN_PROGRESS') { // Poll for status
+async function pollUntilComplete(client: CloudFormationClient, detectionId: string): Promise<StackDriftStatus> {
+  const describeCommand = new DescribeStackDriftDetectionStatusCommand({ StackDriftDetectionId: detectionId });
+
+  while (true) {
     await setTimeout(1000);
     const { DetectionStatus, StackDriftStatus } = await client.send(describeCommand);
-    status = DetectionStatus;
-    if (status !== 'DETECTION_IN_PROGRESS') { // Return result if detection is complete
-      return StackDriftStatus || 'UNKNOWN';
-    }
-  }
 
-  throw new Error(`Error during drift detection for stack ${stackName} (${region})`);
+    const stillDetecting = DetectionStatus === 'DETECTION_IN_PROGRESS';
+    if (stillDetecting)
+      continue;
+
+    return StackDriftStatus || 'UNKNOWN';
+  }
 }
