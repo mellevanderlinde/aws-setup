82c426a5512106af995ef69e404b35042dc044cc
Melle <118454433+mellevanderlinde@users.noreply.github.com>
2026-01-31 19:32:58 +0100

    test: simplify by using it.each() for stack snapshot tests

--- lib/stacks/stacks.test.ts
+++ lib/stacks/stacks.test.ts
@@ -1,6 +1,6 @@
 import { App, Aspects, DefaultStackSynthesizer } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import { beforeEach, describe, expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { RemovalPolicyDestroyAspect } from '../utils/aspects';
 import { BudgetStack } from './budget';
 import { DriftDetectionStack } from './drift-detection';
@@ -9,38 +9,29 @@ import { IdentityCenterStack } from './identity-center';
 
 describe('stacks', () => {
   const email = 'email@example.com';
-  let app: App;
 
-  beforeEach(() => {
-    const defaultStackSynthesizer = new DefaultStackSynthesizer({ generateBootstrapVersionRule: false });
-    app = new App({ defaultStackSynthesizer });
-    Aspects.of(app).add(new RemovalPolicyDestroyAspect());
-  });
+  const defaultStackSynthesizer = new DefaultStackSynthesizer({ generateBootstrapVersionRule: false });
+  const app = new App({ defaultStackSynthesizer });
+  Aspects.of(app).add(new RemovalPolicyDestroyAspect());
 
-  it('garbage collection stack matches snapshot', () => {
-    const stack = new GarbageCollectionStack(app, 'GarbageCollectionStack', { email });
-    const template = Template.fromStack(stack);
-    expect(template).toMatchSnapshot();
-  });
-
-  it('identity center stack matches snapshot', () => {
-    const stack = new IdentityCenterStack(app, 'IdentityCenterStack', {
-      instanceArn: 'instanceArn',
-      userId: 'userId',
-    });
-    const template = Template.fromStack(stack);
-    expect(template).toMatchSnapshot();
-  });
-
-  it('budget stack matches snapshot', () => {
-    const stack = new BudgetStack(app, 'BudgetStack', { email });
-    const template = Template.fromStack(stack);
-    expect(template).toMatchSnapshot();
-  });
-
-  it('drift detection stack matches snapshot', () => {
-    const stack = new DriftDetectionStack(app, 'DriftDetectionStack', { email });
-    const template = Template.fromStack(stack);
-    expect(template).toMatchSnapshot();
+  it.each([
+    [
+      'budget',
+      new BudgetStack(app, 'BudgetStack', { email }),
+    ],
+    [
+      'drift detection',
+      new DriftDetectionStack(app, 'DriftDetectionStack', { email }),
+    ],
+    [
+      'garbage collection',
+      new GarbageCollectionStack(app, 'GarbageCollectionStack', { email }),
+    ],
+    [
+      'identity center',
+      new IdentityCenterStack(app, 'IdentityCenterStack', { instanceArn: 'instanceArn', userId: 'userId' }),
+    ],
+  ])('%s stack matches snapshot', (_name, stack) => {
+    expect(Template.fromStack(stack)).toMatchSnapshot();
   });
 });
