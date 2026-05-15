6a0c613aa8eeefbb6a0156bfd3f4ece26912d2c9
Melle <118454433+mellevanderlinde@users.noreply.github.com>
2026-05-15 12:51:12 +0200

    chore: remove interface

--- lib/utils/instance.ts
+++ lib/utils/instance.ts
@@ -3,12 +3,7 @@ import { assertDefined } from './assert-defined'
 
 const client = new SSOAdminClient({ region: 'eu-west-1' })
 
-interface Instance {
-  instanceArn: string
-  identityStoreId: string
-}
-
-export async function getInstance(): Promise<Instance> {
+export async function getInstance(): Promise<{ instanceArn: string, identityStoreId: string }> {
   const { Instances } = await client.send(new ListInstancesCommand())
   if (Instances?.length !== 1) {
     throw new Error('Unexpected number of instances')
